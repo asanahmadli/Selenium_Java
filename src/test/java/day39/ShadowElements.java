411d3295ee6bf8f84755e70e6d71b1a1bac4d5e0
asana <asenahmedli@gmail.com>
2024-07-01 05:45:45 -0400

    Shadow element,SVG element located and Broken link validation completed

--- src/test/java/day39/ShadowElements.java
+++ src/test/java/day39/ShadowElements.java
@@ -1,5 +1,7 @@
 package day39;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -14,6 +16,17 @@ public class ShadowElements {
         driver.get("https://dev.automationtesting.in/shadow-dom");
         driver.manage().window().maximize();
 
+        SearchContext shadow = driver.findElement(By.cssSelector(".MuiBox-root.css-13o7eu2")).getShadowRoot();
+       String str =  shadow.findElement(By.cssSelector("#shadow-element")).getText();
+        System.out.println(str);
+
+        SearchContext nestedShadow = shadow.findElement(By.cssSelector("#inner-shadow-dom")).getShadowRoot();
+        String str2 = nestedShadow.findElement(By.cssSelector("#nested-shadow-element")).getText();
+        System.out.println(str2);
+
+        SearchContext multiNestedShadow = nestedShadow.findElement(By.cssSelector("#nested-shadow-dom")).getShadowRoot();
+        String str3 = multiNestedShadow.findElement(By.cssSelector("#multi-nested-shadow-element")).getText();
+        System.out.println(str3);
 
     }
 }
