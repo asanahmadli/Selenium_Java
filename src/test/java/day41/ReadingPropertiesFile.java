9b34144bf213a12c1cc235334b721b965995215b
asana <asenahmedli@gmail.com>
2024-07-11 05:11:18 -0400

    Properties file created and logic created for reading keys and values

--- src/test/java/day41/ReadingPropertiesFile.java
+++ src/test/java/day41/ReadingPropertiesFile.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Properties;
+import java.util.*;
 
 public class ReadingPropertiesFile {
 
@@ -19,6 +19,29 @@ public class ReadingPropertiesFile {
         Properties properties = new Properties();
         properties.load(file);
 
+        //Reading data from properties file
+        String url = properties.getProperty("appurl");
+        String email = properties.getProperty("email");
+        String password = properties.getProperty("password");
+        String orderid = properties.getProperty("orderid");
+        String customerid = properties.getProperty("customerid");
 
+        System.out.println(url + " " + email + " " + password + " " + orderid + " " + customerid);
+
+        //reading all the keys from properties file
+        //if all keys string
+        Set<String> keys =  properties.stringPropertyNames();
+        for(String key:keys){
+            System.out.println(key);
+        }
+
+        //when keys are different types
+        Set<Object> keySet = properties.keySet();
+        System.out.println(keySet);
+
+
+        //Reading all the values from properties file
+        Collection<Object> values = properties.values();
+        System.out.println(values);
     }
 }
